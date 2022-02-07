c9d61efa889ae406315d30212cda47837624baa2
Avinash Singh <48854746+avr3085@users.noreply.github.com>
2022-02-07 17:36:57 +0530

    Script

---
+++ CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Transactions;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+        public Transform target;
+        public Vector3 offset;
+        public float followSpeed = 0.1f;
+        private void LateUpdate()
+        {
+                Vector3 desiredPosition = target.position + offset;
+                Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, followSpeed);
+                transform.position = smoothedPosition;
+        }
+}
